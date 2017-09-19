0e649ce88acd77427c1f0d694d940e25961f4c64
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-18 21:24:04 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/shippingcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.a273.sbadajozcs.shippingcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
